b31e11602f0a35e89389bc5bfd8ee855092cb7cd
Art-hack <17ucs036@lnmiit.ac.in>
2019-06-19 16:39:54 +0530

    Adding templates

---
+++ 28_bansal.cpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define mod 1e9+7
+#define FOR(a,c)   for ( int (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( int (a)=(b); (a)<=(c); (a)++)
+#define FORR(a,b,c)  for (  int (a)=(b); (a)>=(c); (a)--)
+typedef long long int ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+#define MP make_pair
+
+int main() {
+ios::sync_with_stdio(0);
+cin.tie(0);
+
+int T;
+cin>>T;
+while(T--){
+    int N;
+    cin>>N;
+    ll a[N];
+    FOR(i,N)
+        cin>>a[i];
+    
+        
+}
+
+
+return 0;
+}
