eb4057219fcd11a4d220c88b089c794c6e3d7ebb
Art-hack <17ucs036@lnmiit.ac.in>
2019-09-06 20:01:38 +0530

    Update art_hack.cpp

--- art_hack.cpp
+++ art_hack.cpp
@@ -14,10 +14,11 @@
 using namespace std;
 #define max(a, b) (a < b? b : a)
 #define min(a, b) ((a>b)?b:a)
-#define mod 1e9+7
+#define mod 1000000007
 #define FOR(a,c)   for ( int (a)=0; (a)<(c); (a)++)
 #define FORL(a,b,c)  for ( int (a)=(b); (a)<=(c); (a)++)
 #define FORR(a,b,c)  for (  int (a)=(b); (a)>=(c); (a)--)
+#define INF 1000000000000000003
 typedef long long int ll;
 typedef vector<int> vi;
 typedef pair<int,int> pi;
